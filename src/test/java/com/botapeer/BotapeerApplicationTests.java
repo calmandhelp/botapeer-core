ec2c6beec9f03cea662449764aa14ab57d341c72
岡本泰輔 <base225001@gmail.com>
2023-01-30 18:44:40 +0900

    properties追加

--- src/test/java/com/botapeer/BotapeerApplicationTests.java
+++ src/test/java/com/botapeer/BotapeerApplicationTests.java
@@ -1,13 +1,17 @@
 package com.botapeer;
 
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class BotapeerApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads(ApplicationContext context) {
+		assertThat(context).isNotNull();
 	}
 
 }
